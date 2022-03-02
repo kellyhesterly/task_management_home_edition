f4982a661d4d57f82e35c80d8337cea47c9749a8
github username <kelly.hesterly@gmail.com>
2022-03-01 22:41:34 -0700

    created login route, session controller and session view

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
 
+  get '/login', to: 'sessions#new'
+
   resources :categories
   resources :chores
   resources :users
