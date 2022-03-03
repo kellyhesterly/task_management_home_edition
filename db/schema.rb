3a30f782efff850993b5e22d7d5350c0efcce3bc
github username <kelly.hesterly@gmail.com>
2022-03-02 22:47:56 -0700

    added admin status to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_045609) do
+ActiveRecord::Schema.define(version: 2022_03_03_054623) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_03_02_045609) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin_status", default: false
   end
 
 end
