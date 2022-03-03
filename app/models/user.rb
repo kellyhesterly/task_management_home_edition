bf8b5affbb6b65ecbb625fc04276fc2096a3834e
github username <kelly.hesterly@gmail.com>
2022-03-02 22:41:05 -0700

    added belongs_to and has_many relationships to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :name, :email, :password, presence: true
+
+    has_many :chores
+    has_many :categories, through: :chores
 end
