c1143f242c29dfd4ec5c76f3b1b358087e3ead4b
github username <kelly.hesterly@gmail.com>
2022-03-07 23:58:15 -0700

    fixed login page (wasn't recognizing accurate password)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,14 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(email: params[:email])
-        if @user && @user.authenticate(password: params[:password])
+        if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
+            session[:email] = @user[:email]
             redirect_to user_path(@user), :flash => { :success => "You have logged in successfully!" }
         else
-            if @user == nil
+            if @user[:email] == nil
                 flash[:error] = "Do not recognize email. Please provide a valid email"
-            else @user[:email] != ""
+            else @user != nil && @user[:email] != ""
                 flash[:error] = "Incorrect password provided"
             end
             render 'new'
