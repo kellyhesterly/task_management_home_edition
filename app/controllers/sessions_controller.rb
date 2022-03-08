95507f0600eca805279918606ea99f8a9d9c74ad
github username <kelly.hesterly@gmail.com>
2022-03-07 23:38:54 -0700

    created flash errors for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,20 @@
 class SessionsController < ApplicationController
+
     def new
         @user = User.new
     end
 
     def create
-        if @user = User.find_by(email: params[:email])
+        @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(password: params[:password])
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            redirect_to user_path(@user), :flash => { :success => "You have logged in successfully!" }
         else
+            if @user == nil
+                flash[:error] = "Do not recognize email. Please provide a valid email"
+            else @user[:email] != ""
+                flash[:error] = "Incorrect password provided"
+            end
             render 'new'
         end
     end
